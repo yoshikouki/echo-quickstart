bb045fdd876b8cf57ea046ada8f002fdfd939fe9
yoshikouki <yoshikouki@pepabo.com>
2021-04-30 23:52:24 +0900

    Create "save" POST

--- server.go
+++ server.go
@@ -13,6 +13,7 @@ func main() {
 
 	e.GET("/users/:id", getUser)
 	e.GET("/show", show)
+	e.POST("/save", save)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
@@ -29,4 +30,10 @@ func show(c echo.Context) error {
 	team := c.QueryParam("team")
 	member := c.QueryParam("member")
 	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
 }
