ccc758b8e1c16c6817ed9030e7e83b6f4ffb9d05
yoshikouki <yoshikouki@pepabo.com>
2021-04-30 23:54:29 +0900

    Update "save" POST

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -33,7 +35,33 @@ func show(c echo.Context) error {
 }
 
 func save(c echo.Context) error {
+	// Get name
 	name := c.FormValue("name")
-	email := c.FormValue("email")
-	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+	// Get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! " + name + "</b>")
+
 }
