a30d8d11bafcfae2cf29f595a0ddeb842989577f
yoshikouki <yoshikouki@pepabo.com>
2021-04-30 23:57:41 +0900

    Create User struct and users POST

--- server.go
+++ server.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
@@ -14,6 +19,15 @@ func main() {
 	})
 
 	e.GET("/users/:id", getUser)
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
 	e.GET("/show", show)
 	e.POST("/save", save)
 
