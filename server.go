b34ed75448d1ecabb253367823e130982c2f47dc
yoshikouki <yoshikouki@pepabo.com>
2021-04-30 23:50:06 +0900

    Create 'show' GET request

--- server.go
+++ server.go
@@ -12,6 +12,7 @@ func main() {
 	})
 
 	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
@@ -21,4 +22,11 @@ func getUser(c echo.Context) error {
 	// User ID from path `users/:id`
 	id := c.Param("id")
 	return c.String(http.StatusOK, id)
+}
+
+// e.GET("/show", show)
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
 }
