630b89a749f358af4cad3113ee073bebe1d76c07
yoshikouki <yoshikouki@pepabo.com>
2021-04-30 23:38:02 +0900

    Create GET Route

--- server.go
+++ server.go
@@ -10,5 +10,15 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!1")
 	})
+
+	e.GET("/users/:id", getUser)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
 }
